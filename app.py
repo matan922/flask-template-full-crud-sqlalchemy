3bbe0e430b385feccc406ea9af42067a26d2e385
matan922 <matanten@gmail.com>
2023-01-17 10:30:29 +0200

    added __init__ to table

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
     #This will help in debugging, this will be shown in the console
     def __repr__(self):
         return '<User %r>' % self.username
